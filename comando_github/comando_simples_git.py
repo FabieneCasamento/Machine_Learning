f2e540e0500debff8c0aaa0ceda0ac971fef6f40
FabieneCasamento <fabieneaulas@gmail.com>
2026-08-09 20:23:49 -0300

    comando git

---
+++ comando_github/comando_simples_git.py
@@ -0,0 +1,6 @@
+(base) meulinux~/Documentos/Fabiene/Repositorio_github/Machine_Learning$ git status
+(base) meulinux~/Documentos/Fabiene/Repositorio_github/Machine_Learning$ git add Dicas_sparks_sql/ML_MODEL_OPERATIONS/ .
+(base) meulinux~/Documentos/Fabiene/Repositorio_github/Machine_Learning$ git commit -m 'fotos para o notebook'
+(base) meulinux~/Documentos/Fabiene/Repositorio_github/Machine_Learning$ git push origin new_branch_fix2 
+
+
